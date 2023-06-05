40261bbe008a11f9f5c1a5440db82cd1596468ee
Paola Andrea Garzon Leon <131317829+pagarzon83@users.noreply.github.com>
2023-06-04 19:48:25 -0500

    carga de archivos centro medico
    carga de archivos proyecto centro medico

---
+++ registro-medicos.js
@@ -0,0 +1,28 @@
+const formularioRegistro = document.getElementById("registro-medicos-form")
+const nombre = document.getElementById("nombre")
+const apellido= document.getElementById("apellido")
+const cedula= document.getElementById("cedula")
+const consultorio= document.getElementById("consultorio")
+const correo = document.getElementById("correo")
+const especialidad = document.getElementById("especialidad")
+
+formularioRegistro.addEventListener('submit',function (event) {
+    event.preventDefault();
+    const objetoMedico = {
+        nombre: nombre.value,
+        apellido: apellido.value,
+        cedula: cedula.value,
+        consultorio: consultorio.value,
+        correo: correo.value,
+        especialidad: especialidad.value,
+    }
+    let medicos = []
+    let localMedicos = localStorage.getItem("medicos")
+    if (localMedicos){
+        medicos =JSON.parse(localMedicos)
+    }
+    medicos.push(objetoMedico)
+    localStorage.setItem("medicos",JSON.stringify(medicos))
+    formularioRegistro.requestFullscreen()
+    alert("Medico registrado con Exito")
+})
